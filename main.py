dceaefd930534b25ec061262b9107609c6b8b045
EdvardSKT <114014282+EdvardSKT@users.noreply.github.com>
2022-11-22 17:23:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,20 +19,18 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 cap = cv2.VideoCapture(0)
 ret, fram = cap.read()
 
-print(ret)
-
 #ser = serial.Serial('COM3', 9600, timeout=1)
 #ser.reset_input_buffer()
-ser2 = serial.Serial('COM5', 9600, timeout=1)
-ser2.reset_input_buffer()
+ser = serial.Serial('COM5', 9600, timeout=1)
+ser.reset_input_buffer()
 
-x_initial, x_angle = 81, 81
-y_initial, y_angle = 75, 75
+x_initial, x_angle = 70, 70
+y_initial, y_angle = 175, 175
 
 x_max = 180
-y_max = 135
+y_max = 300
 x_min = 0
-y_min = 60
+y_min = 80
 
 threshhold_size = 100
 preferred_size_min = 200
@@ -55,7 +53,6 @@ while True:
     center_y = -1
     # Draw the rectangle around each face
     size = 0
-    index = 0
 
     
     for i, (x, y, w, h) in enumerate(faces):
@@ -75,7 +72,7 @@ while True:
     image_center_x = imsize[1] / 2
     image_center_y = imsize[0] / 2
 
-    speed = 1
+    speed = 3
     wheel_speed = 3 #Kjøre frem eller tilbake
 
     size = np.sqrt(size)
@@ -99,10 +96,10 @@ while True:
         if abs(center_y - image_center_y) < 50:
             #print('just right')
             y_angle += 0
-        elif center_y > image_center_y:
+        elif center_y < image_center_y:
             #print('below')
             y_angle -= speed
-        elif center_y < image_center_y:
+        elif center_y > image_center_y:
             #print('above')
             y_angle += speed
 
@@ -112,10 +109,10 @@ while True:
             x_angle -= speed
         elif x_angle < x_initial:
             x_angle += speed
-        if y_angle > y_initial:
-            y_angle -= speed
-        elif y_angle < y_initial:
+        if y_angle < y_initial:
             y_angle += speed
+        elif y_angle > y_initial:
+            y_angle -= speed
     
     
 
@@ -123,9 +120,10 @@ while True:
     x_angle = clamp(x_angle, x_min, x_max)
     y_angle = clamp(y_angle, y_min, y_max)
 
+    #print(y_angle)
     print(wheel_speed)
 
-    #ser.write(bytes(str(x_angle) + "," + str(y_angle) + '\n', 'utf-8'))
-    ser2.write(bytes(str(wheel_speed) + '\n', 'utf-8'))
-    #print(str(x_angle) + "," + str(y_angle))
+    ser.write(bytes(str(x_angle) + "," + str(y_angle) + ";" + str(wheel_speed) + '\n', 'utf-8'))
+
     time.sleep(0.02)
+
